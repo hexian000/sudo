74a86915aee05646619e9bb6440e68ea0f001905
hexian000 <he.xian@163.com>
2019-12-07 12:35:00 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace sudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmd = string.Join(" ", args);
+
+            using (var process = new Process())
+            {
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C " + cmd,
+                    UseShellExecute = true,
+                    CreateNoWindow = true,
+                    Verb = "runas",
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+        }
+    }
+}
